a300fe27bdbc4dc1b5d7237c609415f098b67cb8
coinshot <93342644+coinshot@users.noreply.github.com>
2023-02-16 18:07:54 +0100

    Make dropdown component generic-ish

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,22 +1,32 @@
-import React, { FC, useState } from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
 
-interface DropdownProps {
-  items: string[],
-  onClick: (item: string) => void,
-  selected?: string,
+interface DropdownProps<T> {
+  items: T[],
+  onClick: (item: T) => void,
+  selected?: T | null,
   maxWidth?: number,
   isDropUp?: boolean,
   isDisabled?: boolean
 }
 
-const Dropdown: FC<DropdownProps> = ({items, onClick, selected, maxWidth, isDropUp = false, isDisabled = false}) => {
+type Primitive = string | number | boolean
+
+/*
+ * Dropdown with callback
+ *
+ * Accepts primitive types as the dropdown items, or tuples of [T, T] where the first value is an identifier
+ * and the second value is the printable text.
+ */
+function Dropdown<T extends Primitive | [Primitive, Primitive]>(props: React.PropsWithChildren<DropdownProps<T>>) {
+  const {items, onClick, selected, maxWidth, isDropUp = false, isDisabled = false} = props
+
   const [isActive, setIsActive] = useState(false)
-  const [selectedItem, setSelectedItem] = useState<string | undefined>(selected)
+  const [selectedItem, setSelectedItem] = useState<T | null | undefined>(selected)
 
   const renderItems = React.useMemo(() => {
-    const handleSelect = (e: React.MouseEvent<HTMLDivElement>, item: string) => {
+    const handleSelect = (e: React.MouseEvent<HTMLDivElement>, item: T) => {
       e.preventDefault()
 
       setSelectedItem(item)
@@ -25,14 +35,27 @@ const Dropdown: FC<DropdownProps> = ({items, onClick, selected, maxWidth, isDrop
     }
 
     return items.map((item, i) => {
+      let key, value, equals
+
+      if (Array.isArray(item)) {
+        key = item[0] as string
+        value = item[1]
+        equals = selectedItem && Array.isArray(selectedItem) && selectedItem[0] === item[0]
+
+      } else {
+        key = item as string
+        value = item
+        equals = selectedItem === item
+      }
+
       const style = {
         cursor: "pointer",
-        backgroundColor: selectedItem === item ? "#f7f7f7" : "#ffffff"
+        backgroundColor: equals ? "#f7f7f7" : "#ffffff"
       }
 
       return (
-        <div key={item + i} onClick={(e) => handleSelect(e, item)} className={"dropdown-item" + (selectedItem === item ? " is-active" : "")} style={style}>
-         { item }
+        <div key={key} onClick={(e) => handleSelect(e, item)} className={"dropdown-item" + (equals ? " is-active" : "")} style={style}>
+         { value }
         </div>
       )
     })
@@ -42,7 +65,7 @@ const Dropdown: FC<DropdownProps> = ({items, onClick, selected, maxWidth, isDrop
     <div className={"dropdown" + (isActive && !isDisabled ? " is-active" : "") + (isDropUp ? " is-up" : "")}>
       <div className="dropdown-trigger">
         <div className="button" onClick={() => setIsActive(!isActive && !isDisabled)} aria-haspopup="true" aria-controls="dropdown-menu" style={isDisabled ? {maxWidth: "calc(2em + 8.75px + " + maxWidth + "px)", cursor: "default", pointerEvents: "none"} : {maxWidth: "calc(2em + 8.75px + " + maxWidth + "px)"}}>
-          <span style={{minWidth: 50, maxInlineSize: maxWidth, overflow: "clip"}}> { selectedItem } </span>
+          <span style={{minWidth: 50, maxInlineSize: maxWidth, overflow: "clip"}}> { Array.isArray(selectedItem) ? selectedItem[1] : selectedItem } </span>
           <span className="icon is-small">
             <FontAwesomeIcon icon={faAngleDown} size="sm" color={isDisabled ? "#bbb" : "#363636"} />
           </span>
