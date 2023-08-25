2f0e19d57c4ee36301c0bdb6ea23872871cf74f2
coinshot <93342644+coinshot@users.noreply.github.com>
2023-08-25 13:58:31 +0200

    Add demo video playback

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useLayoutEffect } from 'react'
+import React, { FC, useLayoutEffect, useState } from 'react'
 import { Route, Switch, Link } from "react-router-dom"
 
 import Navbar from 'components/Navbar'
@@ -7,6 +7,8 @@ import Footer from 'components/Footer'
 import Section from 'components/Section'
 
 import screenshotBuilder from 'assets/screenshot-builder.png'
+import webm from 'assets/demo.webm'
+import mp4 from 'assets/demo.mp4'
 
 import { useAuthContext } from 'contexts'
 
@@ -48,9 +50,13 @@ const LandingPage: FC = () => {
 }
 
 const Hero: FC = (props) => {
+  const [modalIsActive, setModalIsActive] = useState(false)
+
   return (
     <section className="hero is-medium">
       <div className="hero-body">
+        { modalIsActive && <Demo onClose={() => setModalIsActive(false)} /> }
+
         <div className="container">
           <div className="columns is-centered is-vcentered px-6">
 
@@ -78,13 +84,38 @@ const Hero: FC = (props) => {
 
             <div className="column is-1" />
             <div className="column is-two-fifths is-hidden-touch">
-              <LaptopFrame image={screenshotBuilder} />
+              <LaptopFrame image={screenshotBuilder} onClick={() => setModalIsActive(true)} />
             </div>
-
           </div>
         </div>
       </div>
     </section>
+  )
+}
+
+interface DemoProps {
+  onClose: () => void
+}
+
+const Demo: FC<DemoProps> = ({ onClose }) => {
+  return (
+    <div className="modal is-active">
+      <div className="modal-background"/>
+      <div className="container">
+        <div className="div is-vcentered">
+          <video controls preload="auto" autoPlay={true}>
+            <source src={webm} type="video/webm" />
+            <source src={mp4} type="video/mp4" />
+
+            <p>
+              Cannot load video
+            </p>
+          </video>
+        </div>
+      </div>
+
+      <button className="modal-close is-large" aria-label="close" onClick={onClose}></button>
+    </div>
   )
 }
 
