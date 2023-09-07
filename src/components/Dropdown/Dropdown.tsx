d30ed414df1b4d9ec06864d06cc405e35fd5c102
coinshot <93342644+coinshot@users.noreply.github.com>
2023-09-07 17:09:50 +0200

    Make Dropdown component searchable

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -5,6 +5,8 @@ import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
 
 import { DataType } from 'types'
 
+import './Dropdown.sass'
+
 
 interface DropdownProps<T> {
   items: T[],
@@ -28,45 +30,74 @@ function Dropdown<T extends Primitive | [Primitive, Primitive] | [string, DataTy
 
   const triggerRef = useRef<HTMLDivElement>(null)
   const contentRef = useRef<HTMLDivElement>(null)
+  const inputRef = useRef<HTMLInputElement>(null)
 
   const [isActive, setIsActive] = useState(false)
+  const [dropUp, setDropUp] = useState(isDropUp)
   const [selectedItem, setSelectedItem] = useState<T | null | undefined>(selected)
+  const [debounce, setDebounce] = useState(Date.now())
+  const [search, setSearch] = useState("")
+
+  const handleActivate = () => {
+    if (!isDisabled && Date.now() >= debounce + 100) {
+      if (inputRef.current && !isActive) {
+        inputRef.current.focus()
+      }
+
+      setIsActive(!isActive)
+    }
+  }
+
+  const handleDeactivate = () => {
+    setDebounce(Date.now())
+    setIsActive(false)
+    setSearch("")
+
+    if (inputRef.current) {
+      inputRef.current.blur()
+    }
+  }
 
   const renderItems = React.useMemo(() => {
     const handleSelect = (e: React.MouseEvent<HTMLDivElement>, item: T) => {
       e.preventDefault()
 
       setSelectedItem(item)
-      setIsActive(false)
       onClick(item)
+      handleDeactivate()
     }
 
-    return items.map((item, i) => {
-      let key, value, equals
+    return items
+      .filter(item => {
+        const value = Array.isArray(item) ? item[1] : item
+        return value.toString().toLowerCase().indexOf(search.toLowerCase()) !== -1
+      })
+      .map((item, i) => {
+        let key, value, equals
 
-      if (Array.isArray(item)) {
-        key = item[0] as string
-        value = item[1]
-        equals = selectedItem && Array.isArray(selectedItem) && selectedItem[0] === item[0]
+        if (Array.isArray(item)) {
+          key = item[0] as string
+          value = item[1]
+          equals = selectedItem && Array.isArray(selectedItem) && selectedItem[0] === item[0]
 
-      } else {
-        key = item as string
-        value = item
-        equals = selectedItem === item
-      }
+        } else {
+          key = item as string
+          value = item
+          equals = selectedItem === item
+        }
 
-      const style = {
-        cursor: "pointer",
-        backgroundColor: equals ? "#f7f7f7" : "#ffffff"
-      }
+        const style = {
+          cursor: "pointer",
+          backgroundColor: equals ? "#f7f7f7" : "#ffffff"
+        }
 
-      return (
-        <div key={key} onMouseDown={(e) => handleSelect(e, item)} className={"dropdown-item" + (equals ? " is-active" : "")} style={style}>
-         { value }
-        </div>
-      )
-    })
-  }, [ items, selectedItem, onClick ])
+        return (
+          <div key={key} tabIndex={0} onMouseDown={(e) => handleSelect(e, item)} className={"dropdown-item" + (equals ? " is-active" : "")} style={style}>
+           { value }
+          </div>
+        )
+      })
+  }, [ items, selectedItem, onClick, search ])
 
   // Manage the portal visibility and location
   useEffect(() => {
@@ -74,9 +105,18 @@ function Dropdown<T extends Primitive | [Primitive, Primitive] | [string, DataTy
       if (isActive && !isDisabled) {
         const rect = triggerRef.current.getBoundingClientRect()
 
-        contentRef.current.style.top = `${rect.bottom + window.pageYOffset}px`
+        if (window.innerHeight - rect.bottom <= 250) {
+          setDropUp(true)
+          contentRef.current.style.top = `${rect.top - 250 + window.pageYOffset}px`
+
+        } else {
+          contentRef.current.style.top = `${rect.bottom + window.pageYOffset}px`
+        }
+
+        contentRef.current.style.width = `${rect.width}px`
         contentRef.current.style.left = `${rect.left + window.pageXOffset}px`
         contentRef.current.style.visibility = "visible"
+
       } else {
         contentRef.current.style.visibility = "hidden"
       }
@@ -84,13 +124,19 @@ function Dropdown<T extends Primitive | [Primitive, Primitive] | [string, DataTy
   }, [ isActive, isDisabled ])
 
   return (
-    <div className={"dropdown" + (isActive && !isDisabled ? " is-active" : "") + (isDropUp ? " is-up" : "")} tabIndex={0} onBlur={() => setIsActive(false)}>
+    <div className={"dropdown" + (isActive && !isDisabled ? " is-active" : "") + (dropUp ? " is-up" : "")} tabIndex={0} onBlur={handleDeactivate}>
       <div ref={triggerRef} className="dropdown-trigger">
         <abbr title={(Array.isArray(selectedItem) ? selectedItem[1] : selectedItem)?.toString()}>
-          <div className="button" onClick={() => setIsActive(!isActive && !isDisabled)} aria-haspopup="true" aria-controls="dropdown-menu" style={isDisabled ? {maxWidth: "calc(2em + 8.75px + " + maxWidth + "px)", cursor: "default", pointerEvents: "none"} : {maxWidth: "calc(2em + 8.75px + " + maxWidth + "px)"}}>
-            <span style={{minWidth: 50, maxInlineSize: maxWidth, overflow: "clip", textOverflow: "ellipsis"}}>
-              { Array.isArray(selectedItem) ? selectedItem[1] : selectedItem }
-            </span>
+          <div className="button" onClick={handleActivate} aria-haspopup="true" aria-controls="dropdown-menu" style={isDisabled ? {maxWidth: "calc(2em + 8.75px + " + maxWidth + "px)", cursor: "default", pointerEvents: "none"} : {maxWidth: "calc(2em + 8.75px + " + maxWidth + "px)"}}>
+            <input
+              ref={inputRef}
+              className="dropdown-input"
+              type="text"
+              value={search}
+              placeholder={Array.isArray(selectedItem) ? selectedItem[1].toString() : selectedItem?.toString()}
+              style={{maxInlineSize: maxWidth}}
+              onChange={e => { setSearch(e.target.value); setIsActive(true) }}
+            />
             <span className="icon is-small">
               <FontAwesomeIcon icon={faAngleDown} size="sm" color={isDisabled ? "#bbb" : "#363636"} />
             </span>
@@ -98,9 +144,9 @@ function Dropdown<T extends Primitive | [Primitive, Primitive] | [string, DataTy
         </abbr>
       </div>
       <Portal>
-        <div ref={contentRef} className="portal">
-          <div className="dropdown-menu" id="dropdown-menu" role="menu" style={isActive && !isDisabled ? {display: "block"} : {}}>
-            <div className="dropdown-content" style={{height: "250px", overflow: "scroll"}}>
+        <div ref={contentRef} className="portal" style={{maxHeight: "250px"}}>
+          <div className="dropdown-menu pt-0" id="dropdown-menu" role="menu" style={isActive && !isDisabled ? {display: "block", maxHeight: "inherit", width: "inherit"} : {}}>
+            <div className="dropdown-content pt-0" style={{maxHeight: "inherit", overflow: "scroll", width: "inherit", height: dropUp ? "250px" : undefined}}>
               { renderItems }
             </div>
           </div>
