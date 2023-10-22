ab843a4678c6dc8c389cb2ad0a499e34a9064240
coinshot <93342644+coinshot@users.noreply.github.com>
2023-10-22 19:23:27 +0200

    Persist onboarding status accross browsers
    Use the backend to store the onboarding status, so that logging into a
    new browser does not reset all the onboarding steps.
    
    Also fixes #13 by timing out the bubbles after 7.5s.

--- src/components/OnboardingSteps/OnboardingSteps.tsx
+++ src/components/OnboardingSteps/OnboardingSteps.tsx
@@ -1,7 +1,8 @@
-import React, { FC, useEffect, useMemo, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import Joyride, { CallBackProps, Step, ACTIONS, EVENTS, STATUS } from 'react-joyride'
 
 import { useKeyStoreContext } from 'contexts'
+import { getCSRFToken } from 'utils/getCSRFToken'
 
 
 interface _Step extends Omit<Step, 'placementBeacon'> {
@@ -18,11 +19,58 @@ const OnboardingSteps: FC<OnboardingStepsI> = ({name, steps}) => {
 
   const [isActive, setIsActive] = useState(false)
   const [retryBackoff, setRetryBackoff] = useState(0)
+  const [cachedSteps, setCachedSteps] = useState<_Step[] | undefined>(undefined)
+  const [timeoutID, setTimeoutID] = useState<number | undefined>()
+
+  const getItem = (key: string) => {
+    return fetch(process.env.REACT_APP_API_BASE_PATH + "/users/settings/" + key, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then((response) => {
+      if (!response.ok) {
+        return Promise.reject(response)
+      } else {
+        return response.json()
+      }
+    }).then(result => {
+      return !!result ? result.value : result
+    }).catch((e) => {
+      console.error(e)
+    })
+  }
+
+  const putItem = (key: string, value: string) => {
+    fetch(process.env.REACT_APP_API_BASE_PATH + "/users/settings", {
+      method: "PUT",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": getCSRFToken()
+      },
+      body: JSON.stringify({
+        "key": key,
+        "value": value
+      })
+    }).catch((e) => {
+      console.error(e)
+    })
+  }
 
   // Only alter the steps on initial load
-  const cachedSteps = useMemo(() => {
-    return steps.slice(parseInt(localStorage.getItem(name) || "0"))
-  }, [name, steps] )
+  useEffect(() => {
+    let isCancelled = false
+
+    getItem(name).then(value => {
+      if (!isCancelled) {
+        setCachedSteps(steps.slice(parseInt(value || "0")))
+      }
+    })
+
+    return () => { isCancelled = true }
+  }, [ name, steps ])
 
   const handleClose = (e: CallBackProps) => {
     if (e.type === EVENTS.TARGET_NOT_FOUND) {
@@ -37,12 +85,18 @@ const OnboardingSteps: FC<OnboardingStepsI> = ({name, steps}) => {
       }
 
     } else if (isActive) {
+      if (e.status === STATUS.RUNNING && e.type === EVENTS.TOOLTIP) {
+        if (timeoutID) {
+          clearTimeout(timeoutID)
+        }
+      }
+
       if (e.type === EVENTS.STEP_AFTER) {
         const newIndex = e.index + (e.action === ACTIONS.PREV ? -1 : 1)
-        localStorage.setItem(name, newIndex.toString())
+        putItem(name, newIndex.toString())
 
       } else if (e.status === STATUS.FINISHED || e.status === STATUS.SKIPPED) {
-        localStorage.setItem(name, "-1")
+        putItem(name, "-1")
         setIsActive(false)
       }
     }
@@ -50,22 +104,36 @@ const OnboardingSteps: FC<OnboardingStepsI> = ({name, steps}) => {
 
   useEffect(() => {
     if (keyStoreIsReady) {
-      setIsActive(parseInt(localStorage.getItem(name) || "0") !== -1)
+      getItem(name).then(value => {
+        setIsActive(parseInt(value || "0") !== -1)
+      })
     }
   }, [ keyStoreIsReady, name ])
 
+  // Disable after a while if the user doesn't interact with the onboarding
+  useEffect(() => {
+    const timeout = window.setTimeout(() => setIsActive(false), 7500)
+    setTimeoutID(timeout)
+
+    return () => clearTimeout(timeout)
+  }, [])
+
   return (
-    <Joyride
-      run={isActive}
-      steps={cachedSteps}
-      styles={{
-        options: {
-          primaryColor: "#e49bcf"
-        }
-      }}
-      continuous={true}
-      callback={handleClose}
-    />
+    <>
+      { (cachedSteps !== undefined) &&
+        <Joyride
+          run={isActive}
+          steps={cachedSteps}
+          styles={{
+            options: {
+              primaryColor: "#e49bcf"
+            }
+          }}
+          continuous={true}
+          callback={handleClose}
+        />
+      }
+    </>
   )
 }
 
